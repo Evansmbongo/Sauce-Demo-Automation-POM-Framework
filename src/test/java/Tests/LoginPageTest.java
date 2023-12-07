b81f8e33060c91196b545387c92090a22b6274ec
Evansmbongo <120010640+Evansmbongo@users.noreply.github.com>
2023-12-07 16:27:57 +0000

    Updated the pom.xml dependencies, andrefactored the LoginPageTest setUp method and added a tearDown method

--- src/test/java/Tests/LoginPageTest.java
+++ src/test/java/Tests/LoginPageTest.java
@@ -2,27 +2,41 @@ package Tests;
 
 import Pages.ItemsPage;
 import Pages.LoginPage;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class LoginPageTest {
     public static WebDriver driver;
-
+    public static String myBrowser = "edge";
     @BeforeMethod
     public static void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/EvansMbongo/Desktop/SeleniumJava/Sauce-Demo" +
-                "-Automation-POM/src/main/resources/chromedriver.exe");
-//        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.setHeadless(true);
-        driver = new ChromeDriver(options);
+        if (myBrowser.equals("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        }
 
-        driver.get("https://www.saucedemo.com/");
+       if (myBrowser.equals("firefox")) {
+           FirefoxOptions options = new FirefoxOptions();
+           options.addArguments("--headless");
+           driver = new FirefoxDriver(options);
+        }
+       else if (myBrowser.equals("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments("--headless");
+            driver = new EdgeDriver(options);
+        }
+
+       driver.get("https://www.saucedemo.com/");
     }
 
     @Test
@@ -30,5 +44,10 @@ public class LoginPageTest {
         LoginPage.username(driver).sendKeys("standard_user");
         LoginPage.password(driver).sendKeys("secret_sauce");
         LoginPage.loginButton(driver).click();
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.quit();
     }
 }
